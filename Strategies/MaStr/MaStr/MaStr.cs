5e015d84c0f2a8e5b7a106d944c4d81d42c3b3cd
yacper <yacper@gmail.com>
2023-03-24 22:57:42 +0800

    .

--- Strategies/MaStr/MaStr/MaStr.cs
+++ Strategies/MaStr/MaStr/MaStr.cs
@@ -3,7 +3,7 @@
     author:		rush
     email:		
 *********************************************************************/
- 
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,108 +24,162 @@ using Neo.Common.Scripts;
 using Neo.Common.Symbols;
 using RLib.Base;
 using RLib.Graphics;
- 
+
 namespace Neo
 {
-	[Strategy(Group = "Trends")]
+[Strategy(Group = "Trends")]
 public class MvaStr : Strategy
-	{
+{
 #region 用户Paras
- 
-		[Parameter, Range(5, 6), DefaultValue(5)]
-	public int QuickPeriods
-		{
-			get; set;
-		}
- 
-		[Parameter, Range(10, 11), DefaultValue(20)]
-	public int SlowPeriods
-		{
-			get; set;
-		}
- 
-		[Parameter, Range(1, 2), DefaultValue(1)]
-	public double Quantity
-		{
-			get; set;
-		}
- 
-		[Output, Stroke("green")]
-	public IIndicatorDatas QuickMaResult
-		{
-			get; set;
-		}
- 
-		[Output, Stroke("blue")]
-	public IIndicatorDatas SlowMaResult
-		{
-			get; set;
-		}
+
+    [Parameter, Range(1, int.MaxValue), DefaultValue(5)]
+    public int QuickPeriods { get; set; }
+
+    [Parameter, Range(1, int.MaxValue), DefaultValue(20)]
+    public int SlowPeriods { get; set; }
+
+    [Parameter, Range(1, int.MaxValue), DefaultValue(1)]
+    public double Quantity { get; set; }
+
+    [Output, Stroke("green")]
+    public IIndicatorDatas QuickMaResult { get; set; }
+
+    [Output, Stroke("blue")]
+    public IIndicatorDatas SlowMaResult { get; set; }
 #endregion
- 
-		protected override void OnStart()
-		{
-			Info("OnStart");
- 
-			QuickMa_ = CreateScript<MVA>(Bars.Closes, QuickPeriods);
-			QuickMaResult.Stroke = new Stroke()
-			{
-				Color = Colors.Yellow
-			};
-			SlowMa_ = CreateScript<MVA>(Bars.Closes, SlowPeriods);
-		}
- 
-		protected override void OnData(ISource source, int index)
-		{
-			//Info($"{this.LongName} OnData {source.ToString()}[{index}]:{source[index]}");
- 
-			if(QuickPeriods >= SlowPeriods)
-				return;
-			
-			QuickMaResult[index] = QuickMa_.Result[index];
-			SlowMaResult[index] = SlowMa_.Result[index];
- 
-			var longPosition = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Buy);
-			var shortPosition = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Sell);
- 
-			if (QuickMa_.Result.CrossOver(SlowMa_.Result) && longPosition == null)
-			{
-				if (shortPosition != null)
-					CloseTrade(shortPosition);
-				ExecuteMarketOrder(Symbol.Contract, ETradeDirection.Buy, Quantity);
-			}
-			else if (QuickMa_.Result.CrossDown(SlowMa_.Result) && shortPosition == null)
-			{
-				if (longPosition != null)
-					CloseTrade(longPosition);
-				ExecuteMarketOrder(Symbol.Contract, ETradeDirection.Sell, Quantity);
-			}
-		}
- 
- 
-		protected void ExecuteMarketOrder(SymbolContract contract, ETradeDirection dir, double quantity, string label = null)
-		{
-			var oi = new MarketOrderInfo(contract, dir, quantity)
-			{
-				Label = label
-			};
- 
-			this.TradingAccount.PlaceOrder(oi);
-		}
- 
-		protected void CloseTrade(ITrade t)
-		{
-			var oi = new MarketOrderInfo(t.Symbol.Contract, t.Direction.Reverse(), t.Quantity)
-			{
-				CloseTradeId = t.Id,
-			OpenClose = EOpenClose.Close
-			};
- 
-			this.TradingAccount.PlaceOrder(oi);
-		}
- 
- 
-		protected MVA QuickMa_;
-		protected MVA SlowMa_;
-	}
+
+    protected override void OnStart()
+    {
+        if (QuickPeriods >= SlowPeriods)
+        {
+            Error("Bad Args");
+            Stop();
+            return;
+        }
+
+        QuickMa_ = CreateScript<MVA>(Bars.Closes, QuickPeriods);
+        QuickMaResult.Stroke = new Stroke()
+        {
+            Color = Colors.Yellow
+        };
+        SlowMa_ = CreateScript<MVA>(Bars.Closes, SlowPeriods);
+
+        //LongTrade_ = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Buy && p.Comment == Label);
+        //ShortTrade_ = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Sell && p.Comment == Label);
+    }
+
+    protected override void OnData(ISource source, int index)
+    {
+        //Info($"{this.LongName} OnData {source.ToString()}[{index}]:{source[index]}");
+
+        QuickMaResult[index] = QuickMa_.Result[index];
+        SlowMaResult[index]  = SlowMa_.Result[index];
+
+        if (!IsHistoryOver)
+            return;
+
+        if (OrderSending)
+            return;
+
+        LongTrade_ = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Buy && p.Comment == Label);
+        ShortTrade_ = TradingAccount.Trades.FirstOrDefault(p => p.Code == Symbol.Code && p.Direction == ETradeDirection.Sell && p.Comment == Label);
+
+        if (QuickMa_.Result.CrossOver(SlowMa_.Result))
+        {
+            if (ShortTrade_ != null)
+                CloseTrade(ShortTrade_);
+            if(LongTrade_ == null)
+                ExecuteMarketOrder(Symbol.Contract, ETradeDirection.Buy, Quantity, Label);
+        }
+        else if (QuickMa_.Result.CrossDown(SlowMa_.Result))
+        {
+            if (LongTrade_ != null)
+                CloseTrade(LongTrade_);
+            if(ShortTrade_ == null)
+                ExecuteMarketOrder(Symbol.Contract, ETradeDirection.Sell, Quantity, Label);
+        }
+    }
+
+
+    protected void ExecuteMarketOrder(SymbolContract contract, ETradeDirection dir, double quantity, string label = null)
+    {
+        var oi = new MarketOrderInfo(contract, dir, quantity)
+        {
+            Label = label
+        };
+
+        var ret = this.TradingAccount.PlaceOrder(oi, (e) =>
+        {
+            if (e.IsSuccessful)
+            {
+                if (e.Trade != null)
+                {
+                    if (e.Trade.Direction == ETradeDirection.Buy)
+                        LongTrade_ = e.Trade;
+                    else
+                        ShortTrade_ = e.Trade;
+                }
+            }
+
+            if (dir == ETradeDirection.Buy)
+                LongSending_ = false;
+            else
+                ShortSending_ = false;
+        });
+
+        if (ret.IsExecuting)
+        {
+            if (dir == ETradeDirection.Buy)
+                LongSending_ = true;
+            else
+                ShortSending_ = true;
+        }
+    }
+
+    protected void CloseTrade(ITrade t)
+    {
+        var oi = new MarketOrderInfo(t.Symbol.Contract, t.Direction.Reverse(), t.Quantity)
+        {
+            CloseTradeId = t.Id,
+            OpenClose    = EOpenClose.Close
+        };
+        var ret =this.TradingAccount.PlaceOrder(oi, (e) =>
+        {
+            if (e.IsSuccessful)
+            {
+                if (t.Direction == ETradeDirection.Buy)
+                    LongTrade_ = null;
+                else
+                    ShortTrade_ = null;
+            }
+
+            if (t.Direction == ETradeDirection.Buy)
+                LongClosing_ = false;
+            else
+                ShortClosing_ = false;
+        });
+
+        if (ret.IsExecuting)
+        {
+            if (t.Direction == ETradeDirection.Buy)
+                LongClosing_ = true;
+            else
+                ShortClosing_ = true;
+        }
+    }
+
+    protected string Label => LongName + Id;
+
+    protected MVA  QuickMa_;
+    protected MVA  SlowMa_;
+
+    protected ITrade LongTrade_;
+    protected bool   LongSending_;
+    protected bool   LongClosing_;
+    protected ITrade ShortTrade_;
+    protected bool   ShortSending_;
+    protected bool   ShortClosing_;
+
+    protected bool OrderSending=>LongSending_|LongClosing_|ShortClosing_|ShortSending_;
+}
 }
