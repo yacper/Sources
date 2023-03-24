2a1f39921f8e2ee276fdccd07fc4a0b08f21c522
yacper <yacper@gmail.com>
2023-03-24 16:26:05 +0800

    .

---
+++ Indicators/TimeFrame/TimeFrame/TimeFrame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics;
+using Neo.Api;
+using Neo.Api.Alert;
+using Neo.Api.Attributes;
+using Neo.Api.Charts;
+using Neo.Api.MarketData;
+using Neo.Api.Providers;
+using Neo.Api.Scripts;
+using Neo.Api.Symbols;
+using Neo.Common.Scripts;
+using RLib.Base;
+using RLib.Graphics;
+
+namespace Neo.Scripts.Custom
+{
+[Indicator(Group = "Utils")]
+public class TimeFrame : Indicator
+{
+#region 用户参数
+    [Parameter, Display(Name = "TimeBegin"), DefaultValue("2:00:00")]
+    public TimeSpan TimeBegin { get; set; }
+  
+    [Parameter, Display(Name = "TimeEnd"), DefaultValue("9:00:00")]
+    public TimeSpan TimeEnd { get; set; }
+
+    [Parameter, Display(Name = "LineStroke"), Stroke("#ffa0a4ad", DashPattern = "1,2")]
+    public Stroke LineStroke { get; set; }
+#endregion
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnData(ISource source, int index)
+    {
+        var t = (source[index] as IBar).Time.TimeOfDay;
+
+        if (t == TimeBegin || t==TimeEnd)
+        {
+            Chart.MainArea.DrawVerticalLine(t.ToString(), (source[index] as IBar).Time, LineStroke);
+        }
+    }
+}
+}
---
+++ Strategies/SamplePlaceOrder/SamplePlaceOrder/SamplePlaceOrder.cs
@@ -0,0 +1,81 @@
+﻿/********************************************************************
+    created:	2017/4/14 15:14:23
+    author:		rush
+    email:		
+*********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics;
+using Neo.Api;
+using Neo.Api.Attributes;
+using Neo.Api.MarketData;
+using Neo.Api.Scripts;
+using Neo.Api.Symbols;
+using Neo.Api.Trading;
+using Neo.Common.Scripts;
+using Neo.Common.Symbols;
+using RLib.Base;
+using RLib.Graphics;
+
+namespace Neo.Scripts.Custom
+{
+[Strategy(Group="Samples")]
+public class SamplePlaceOrder : Strategy
+{
+    protected override void OnStart()
+    {
+		//PlaceMarketOrder(ETradeDirection.Buy, 1, "sample");
+        PlaceLimitOrder(ETradeDirection.Buy, 1, 1800, nameof(SamplePlaceOrder));
+    }
+
+    protected void PlaceMarketOrder(ETradeDirection dir, double quantity, string label = null)
+    {
+        var oi = new MarketOrderInfo(Symbol.Contract, dir, quantity)
+        {
+            Label = label
+        };
+ 
+        this.TradingAccount.PlaceOrder(oi);
+    }
+
+    protected void PlaceLimitOrder(ETradeDirection dir , double quantity, double price, string label = null)
+    {
+        LimitOrderInfo orderInfo = new LimitOrderInfo(Symbol.Contract, dir, price, quantity, ETIF.GTC)
+            { Label = label };
+
+        // 使用chart当前symbol对应的provider的主账户
+        TradeOperation operation = TradingAccount.PlaceOrder(orderInfo, tradeResult =>
+        {// 执行完毕回调
+            if (tradeResult.IsSuccessful)
+                Logger.Info($"限价单发送成功 {tradeResult}");
+            else
+                Logger.Info($"限价单发送失败 {tradeResult}");
+        });
+
+        if (operation.IsExecuting)
+        {
+            Logger.Info("Operation Is Executing");
+        }
+
+    }
+
+    protected void CloseOrder(ITrade t)
+    {
+        var oi = new MarketOrderInfo(t.Symbol.Contract, t.Direction.Reverse(), t.Quantity)
+        {
+            CloseTradeId = t.Id,
+            OpenClose    = EOpenClose.Close
+        };
+
+        this.TradingAccount.PlaceOrder(oi);
+    }
+
+}
+}
